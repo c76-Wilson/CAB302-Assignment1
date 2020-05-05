02d1784e9634b17fa15a4d38dc390a0abf9af3a8
PI\Pierce <pierceevans4@gmail.com>
2020-05-05 15:19:09 +1000

    Fixing Dev Misc HEAD and Remote Merge

--- Billboard/src/ControlPanel/Panel.java
+++ Billboard/src/ControlPanel/Panel.java
@@ -1,8 +1,10 @@
 package ControlPanel;
 import javax.swing.*;
+import java.awt.*;
+import java.io.IOException;
 
-public class Panel extends JFrame {
-    private static JFrame panel;
+public class Panel extends JFrame implements Runnable {
+    private JFrame panel;
     private JPanel panelMain;
     private JLabel firstSchedule;
     private JLabel firstLabel;
@@ -10,7 +12,7 @@ public class Panel extends JFrame {
     private JPanel settingsPane;
     private JLabel secondLabel;
     private JLabel secondSchedule;
-    private static JLabel mainViewer;
+    private JLabel mainViewer;
     private JComboBox selectWeekCombo;
     private JLabel fourthLabel;
     private JLabel thirdLabel;
@@ -24,37 +26,72 @@ public class Panel extends JFrame {
     private JLabel seventhLabel;
     private JLabel seventhSchedule;
 
-    public static void setFrameDefaults(){
-        JFrame frame = panel;
-        panel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        panel.setSize(1280, 720);
-        panel.setLocation(200, 200);
+    // Frame Variables
+    Dimension frameSize = Toolkit.getDefaultToolkit().getScreenSize();
+    private int WIDTH = frameSize.width;
+    private int HEIGHT = frameSize.height;
+    Dimension frame = new Dimension(WIDTH,HEIGHT);
+
+    public Panel(String title) throws IOException {
+        super(title);
+
+        run();
     }
 
-    public static void renderImage(String image_dir){
-        JLabel mainView = mainViewer;
+    /**
+     * This Method created the Gui Frame Defaults
+     */
+    public void setFrameDefaults() {
+        setPreferredSize(frame);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(1280, 720);
+        setLocation(200, 200);
+        panelMain = returnPanel();
+        editingPane = returnPanel();
+        settingsPane = returnPanel();
+        setVisible(true);
+    }
+
+    private JPanel returnPanel() {
+        JPanel dummy = new JPanel();
+
+        return dummy;
+    }
+
+    public void renderImage(String image_dir) {
+        mainViewer = setLabel();
         ImageIcon icon = new ImageIcon(Panel.class.getResource(image_dir));
-        mainView.setIcon(icon);
+        mainViewer.setIcon(icon);
     }
 
-    static class panelTest extends Panel {
-        public static void main(String[] args) {
-            setFrameDefaults();
-            renderImage("C:/Users/Pierce/Pictures/itsame.png");
-        }
+    private JLabel setLabel() {
+        JLabel temp = new JLabel();
 
-        /**
-         * public static void main(String[] args) {
-         *     SwingUtilities.invokeLater(new Runnable() {
-         *         public void run() {
-         *             FileExtractorGUI gui = new FileExtractorGUI();
-         *             JFrame frame = new JFrame();
-         *             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         *             frame.getContentPane().add(gui);
-         *             frame.pack();
-         *             frame.setVisible(true);
-         *         }
-         *     });
-         * }
-         */
+        return temp;
     }
+
+    /**
+     * public static void main(String[] args) {
+     * SwingUtilities.invokeLater(new Runnable() {
+     * public void run() {
+     * FileExtractorGUI gui = new FileExtractorGUI();
+     * JFrame frame = new JFrame();
+     * frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     * frame.getContentPane().add(gui);
+     * frame.pack();
+     * frame.setVisible(true);
+     * }
+     * });
+     * }
+     */
+    @Override
+    public void run() {
+        setFrameDefaults();
+    }
+
+    public static void main(String[] args) throws IOException{
+        JFrame first = new Panel("First Test");
+        ((Panel) first).renderImage("C:/Users/Pierce/Pictures/itsame.png");
+    }
+}
+
